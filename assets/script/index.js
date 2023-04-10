15af31e96137c9081fe127a016fbb87cfdd6e474
Shubham <shubham11729@gmail.com>
2023-04-10 13:08:49 -0500

    changes done

--- assets/script/index.js
+++ assets/script/index.js
@@ -2,69 +2,53 @@
 
 const flyToBtn = document.querySelector('#fly-btn');
 const trackBtn = document.querySelector('.track');
+const overlay = document.querySelector('.overlay');
+const loading = document.querySelector('.loading');
+
 
 mapboxgl.accessToken =
   "pk.eyJ1IjoiMWFtc2h1YmhhbWhlcmUiLCJhIjoiY2xnNWNxeGlrMDI3eDNkbjlqc2U0dmExZiJ9.a_PifubAnkVodM62G7_JiQ"
 
-
-const options = {
-  enableHighAccuracy: true
-}
-
-function errorHandler(){
-  console.log('Unable to retrieve your location ');
-}
-
-let lat, long;
-function getLocation(position){
-  console.log(position);
-  setupMap([position.coords.longitude, position.coords.latitude])
-
-  const { latitude, longitude } = position.coords;
-  lat = latitude;
-  long = longitude;
-}
-
-let map; // Declare the map variable outside the setupMap function
-
-
-  if (navigator.geolocation){
-    navigator.geolocation.getCurrentPosition(getLocation, errorHandler, options);
-    flyToBtn.style.visibility = 'visible';
-  } else {
-    console.log(`Geolocation is not supported by your browser`);
-  }
-
-
-flyToBtn.addEventListener('click', () => {
-  map.flyTo({
-    center: [long , lat],
-    duration: 3,
-    easeLinearity: 0.5,
-    essential: true
+  const map = new mapboxgl.Map({
+    center: [0, 0],
+    container: 'map',
+    pitch: 30,
+    style: 'mapbox://styles/mapbox/streets-v11',
+    zoom: 15
   });
   
-})
-
-function setupMap(center) {
-  map = new mapboxgl.Map({
-    container: "map",
-    style: "mapbox://styles/mapbox/streets-v11",
-    center: center,
-    zoom: 15
-  })
-
-  const nav = new mapboxgl.NavigationControl()
-  map.addControl(nav)
-
-  const directions = new MapboxDirections({
-    accessToken: mapboxgl.accessToken
-  })
-
-  map.addControl(directions, "top-left")
-  const marker = new mapboxgl.Marker({
-    color: "#3c57ff",
-    draggable: true
-  }).setLngLat(center)
-    .addTo(map);
-}
+  map.dragPan.disable();
+  map.keyboard.disable();
+  map.scrollZoom.disable();
+  map.doubleClickZoom.disable();
+  map.touchZoomRotate.disable();
+  
+  const marker = new mapboxgl.Marker();
+  
+  function getLocation(position) {
+    const {longitude, latitude} = position.coords;
+    
+    if (longitude && latitude) {
+      map.setCenter([longitude, latitude]);
+      marker.setLngLat([longitude, latitude]).addTo(map);
+      setTimeout(() => {
+        overlay.style.display = 'none';
+      }, 500);
+    };
+  };
+  
+  function errorHandler(event) {
+    loading.style.animationPlayState = 'paused';
+    console.log(event.message);
+  };
+  
+  const options = {
+    enableHighAccuracy: true,
+    maximumAge: 0
+  }
+  
+  if (navigator.geolocation) {
+    navigator.geolocation.watchPosition(getLocation, errorHandler, options);
+  } else {
+    console.log('Geolocation is not supported on this browser');
+  };
